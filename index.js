e7cd9ab37ec27bbabbbe23c225f10ec2ef2eb298
gianlucaguarini <gianluca.guarini@gmail.com>
2020-06-20 10:41:05 +0200

    updated: index file

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 
 // Set options as a parameter, environment variable, or rc file.
 // eslint-disable-next-line no-global-assign
-require = require("esm")(module/* , options */)
-module.exports = require("./src/index.js")
+require = require('esm')(module) // eslint-disable-line
+const createRiotAPI = require('./src/index.js')
+
+if (module.parent) {
+  module.exports = createRiotAPI
+/* istanbul ignore next */
+} else createRiotAPI.default(process.argv)
